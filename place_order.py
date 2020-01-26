a6c6e88b1bc4cb888ae359f6afec99905ddd6490
mromeroval <mauricio.romero@outlook.com>
2020-01-26 21:11:50 +0100

    Add function for checking supplier by id

--- place_order.py
+++ place_order.py
@@ -1,20 +1,28 @@
-
 #%%
+#!/usr/bin/python3
+
 import sqlite3
 conn = sqlite3.connect('orders.db')
 cursor = conn.cursor()
 
-supplierId = input("Type Supplier ID")
+# Check if supplier exist
+def checkSupplier(supplierId):
+  cursor.execute('SELECT * from suppliers WHERE supplierId=?', (supplierId,))
+  row = cursor.fetchone()
+  if row is None:
+    print("FAIL NO_SUPPLIER")
+  else:
+    id = row[0]
+    name = row[1]
+    print ("Supplier ID: " + str(id))
+    print ("Supplier Name: " + str(name))
+    conn.commit()
 
-cursor.execute('SELECT * from suppliers WHERE supplierId=?', (supplierId,))
-row = cursor.fetchone()
+orderInput = input("place_order ")
+# c3e470 14fcbd,cc3c15
+# q1w2e3 14fcbd,cc3c15
 
-if row is None:
-  print("Supplier Doesn't Exist")
-else:
-  id = row[0]
-  name = row[1]
-  print ("Supplier ID: " + str(id))
-  print ("Supplier Name: " + str(name))
-
-conn.commit()
+orderArray = orderInput.split()
+id = orderArray[0]
+products = orderArray[1]
+checkSupplier(id)
